df3d2ad88fad79ac66e949605046493b3283ee5b
zirpin <zirpin@gmail.com>
2021-03-25 19:02:59 +0100

    data con ejemplos

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into users (name, surname, dni, is_active) values ('name 1', 'surname 1', '10000A', true);
+
+insert into tasks (title, description, finished) values ('task 1', 'desc 1', false);
+
+insert into tags(name) values ('tag 1');
+
+insert into billing_info (street, province, postal_code, country) values ('street 1', 'province 1', '10000', 'country1');
