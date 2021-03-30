8338fa8413cbcc060e61b5d441aa9aa0508254f2
zirpin <zirpin@gmail.com>
2021-03-30 12:44:49 +0200

    create findbyname

--- src/main/java/com/example/proyecto5hibernate/repository/UserRepository.java
+++ src/main/java/com/example/proyecto5hibernate/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.example.proyecto5hibernate.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    public User findByName(String name);
 }
 
 
